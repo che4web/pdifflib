02af26fd6cc277220193a972fa2efa72da30b24d
che <che-email@yandex.ru>
2023-09-30 12:03:05 +0500

    git

--- src/io.rs
+++ src/io.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use ndarray::{ Array2};
+use::hdf5;
 pub fn write_mat(phi: &Array2<f64>,name:String,h:f64){
     let mut file = File::create(name+".dat").unwrap();
     for j in  0..phi.shape()[1]{
@@ -27,5 +28,44 @@ pub fn write_stage(data: Vec<&Array2<f64>>,header:Vec<String>,name:String,h:f64)
         }
     }
 }
+
+
+pub fn write_stage_h5(data: Vec<&Array2<f64>>,header:Vec<String>,name:String,h:f64)->hdf5::Result<()>{
+    let file = hdf5::File::open_rw("./storag.h5").unwrap();
+    let group_name = format!("map/{}",name);
+    let group_r = file.create_group(group_name.as_str());
+    let group = group_r?;
+    for i in 0..data.len(){
+        let builder = group.new_dataset::<f32>().shape(data[i].shape());
+        let builder = builder.blosc_zstd(9, true);
+        let dataset = builder.create(header[i].as_str())?;
+        let res = dataset.write((data[i]));
+        
+    }
+    Ok(())
+}
+pub fn read_stage_h5(data: Vec<&Array2<f64>>,header:Vec<String>)->hdf5::Result<((Vec<Array2<f64>>,f64))>{
+    let file = hdf5::File::open_rw("./storag.h5").unwrap();
+    let groups_list =  file.group("map")?.groups()?;
+    let name = groups_list.last().unwrap().name();
+    let time_s:Vec<&str> = name.split('=').collect();
+    let time:f64 = time_s[1].parse().unwrap();
+    println!("{:?}",name);
+    println!("{:?}",time);
+
+    let group_name = format!("{}",name);
+    println!("{:?}",group_name);
+    let group_r = file.group(group_name.as_str());
+    let group = group_r?;
+    let mut ret_data: Vec<Array2<f64>> =Vec::new() ;
+    for i in 0..data.len(){
+        let dataset = group.dataset(header[i].as_str())?;
+        ret_data.push(dataset.read()?);
+        
+    }
+    Ok((ret_data,time))
+}
+
+
 
 
--- src/system.rs
+++ src/system.rs
@@ -3,20 +3,49 @@ use csv::Writer;
 use std::time::{Instant};
 use std::fs::File;
 use std::fs;
+use std::fs::OpenOptions;
+const LOG_NAME : &str = "foo.csv";
 
 pub trait System{
     fn next_step(&mut self,_dt:f64,time:f64);
     fn write_mat(&mut self, time:f64);
+    fn read_mat(&mut self )->f64;
+    fn write_last(&mut self);
     fn boundary_condition(&mut self);
     fn get_max_time(&self)->f64;
     fn log_params(&self, wrt:&mut Writer<File>, time:f64);
+
+
     fn initial_condition(&mut self);
     fn get_DT(&self) ->f64;
-    fn solve(&mut self){
-        self.initial_condition();
-        let mut wtr: Writer<std::fs::File> = Writer::from_path("foo.csv").unwrap();
-
+    fn solve(&mut self,save_map:bool){
         let mut time= 0.0;
+
+        let mut wtr: Writer<std::fs::File>; 
+
+        {
+            let storage_path = "./storag.h5";
+            match fs::metadata(storage_path) {
+                Ok(_)=>{
+                    time = self.read_mat();
+                    let mut file = OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .open(LOG_NAME)
+                        .unwrap();
+                    wtr = Writer::from_writer(file);
+
+
+                }
+                Err(_)=>{
+                    let _file = hdf5::File::create("./storag.h5").unwrap();
+                    self.initial_condition();
+                    wtr = Writer::from_path(LOG_NAME).unwrap();
+
+                }
+            }
+        }
+
         let start = Instant::now();
         let mut  time_i=0;
         let r =fs::create_dir("res");
@@ -24,7 +53,7 @@ pub trait System{
         let DT = self.get_DT();
 
         while time < self.get_max_time(){
-            for _i in 0..1000{
+            for _i in 0..5000{
                 self.next_step(DT,time);
                 time+=DT;
             }
@@ -33,7 +62,9 @@ pub trait System{
             if time_i %10 == 0{
                 let duration = start.elapsed();
                 println!("{:0.2},{:?}",time/self.get_max_time(),duration) ;
-                self.write_mat(time);
+                if save_map {
+                    self.write_mat(time);
+                }
             }
         }
         println!("==============");
